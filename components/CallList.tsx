8921b61e784e66576fb9b9c1f9442ff0c21aff05
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-02-02 22:47:33 +0330

    Added upcoming, previous and recordings pages

---
+++ components/CallList.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import { useGetCalls } from "@/hooks/useGetCalls"
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react";
+import MeetingCard from "./MeetingCard";
+import Loader from "./Loader";
+import { useToast } from "@/hooks/use-toast";
+
+const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
+  const { endedCalls, upcomingCalls, isLoading, CallRecordings } = useGetCalls();
+  const router = useRouter();
+  const [recordings, setRecordings] = useState<CallRecording[]>([])
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(isLoading);
+  },[isLoading])
+
+  const { toast } = useToast()
+
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  }
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'You do not have any previous calls';
+      case 'recordings':
+        return 'You do not have any calls recorded';
+      case 'upcoming':
+        return 'You do not have any upcoming calls';
+      default:
+        return '';
+    }
+  }
+
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      setLoading(true)
+      try {
+        const callData = await Promise.all(CallRecordings.map((meeting) => meeting.queryRecordings()))
+        const recordings = callData
+          .filter(call => call.recordings.length > 0)
+          .flatMap(call => call.recordings);
+
+        setRecordings(recordings)
+      } catch (e: any) {
+        toast({ title: 'There was an error. Try again later.' })
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (type === 'recordings') fetchRecordings()
+  }, [type, CallRecordings])
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  if (loading || isLoading) return <Loader />
+
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {
+        calls && calls.length > 0 ? (calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === 'ended' ? '/icons/previous.svg' : type === 'upcoming' ? '/icons/upcoming.svg' : '/icons/recordings.svg'
+            }
+            title={(meeting as Call).state?.custom.description.substring(0, 26) || (meeting as CallRecording).filename.substring(0, 26) || 'No description'}
+            date={(meeting as Call).state?.startsAt?.toLocaleString() || (meeting as CallRecording).start_time.toLocaleString()}
+            isPreviousMeeting={type === 'ended'}
+            buttonIcon={type === 'recordings' ? '/icons/play.svg' : undefined}
+            buttonText={type === 'recordings' ? 'Download' : 'Start'}
+            link={type === 'recordings' ? (meeting as CallRecording).url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`}
+            handleClick={type === 'recordings' ? () => router.push(`${(meeting as CallRecording).url}`) : () => router.push(`/meeting/${(meeting as Call).id}`)}
+          />
+        ))) : (
+          <h1>{noCallsMessage}</h1>
+        )
+      }
+    </div>
+  )
+}
+
+export default CallList
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,8 @@ import MeetingModal from "./MeetingModal"
 import { useUser } from "@clerk/nextjs"
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
 import { useToast } from "@/hooks/use-toast"
+import { Textarea } from "./ui/textarea"
+import ReactDatePicker from "react-datepicker";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -23,7 +25,7 @@ const MeetingTypeList = () => {
   const { toast } = useToast()
 
   const createMeeting = async () => {
-    if(!values.datetime) {
+    if (!values.datetime) {
       toast({ title: "Please select a date and time" })
     }
     if (!client || !user) return;
@@ -61,6 +63,8 @@ const MeetingTypeList = () => {
     }
   }
 
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -92,6 +96,50 @@ const MeetingTypeList = () => {
         onClick={() => router.push('/recordings')}
       />
 
+      {
+        !callDetails ? (
+          <MeetingModal
+            isOpen={meetingState === 'isScheduleMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title="Schedule a meeting"
+            className="text-center"
+            buttonText="Start Meeting"
+            handleClick={createMeeting}
+          >
+            <div className="flex flex-col gap-2.5">
+              <label className="text-base text-normal leading-[22px] text-sky-2">Add a Description</label>
+              <Textarea className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0" onChange={(e) => setValues({ ...values, desc: e.target.value })} />
+            </div>
+            <div className="flex w-full flex-col gap-2.5">
+              <label className="text-base text-normal leading-[22px] text-sky-2">Select data and time</label>
+              <ReactDatePicker
+                selected={values.datetime}
+                onChange={(date) => setValues({ ...values, datetime: date! })}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="MMMM d, yyyy h:mm aa"
+                className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+              />
+            </div>
+          </MeetingModal>
+        ) : (
+          <MeetingModal
+            isOpen={meetingState === 'isScheduleMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title="Schedule a meeting"
+            className="text-center"
+            buttonText="Copy Meeting Link"
+            handleClick={() => {
+              navigator.clipboard.writeText(meetingLink)
+              toast({ title: "Link compied" })
+            }}
+            image="/icons/checked.svg"
+            buttonIcon="/icons/copy.svg"
+          />
+        )
+      }
       <MeetingModal
         isOpen={meetingState === 'isInstantMeeting'}
         onClose={() => setMeetingState(undefined)}
