76b279db54bd4be188b42d4fea29b210951c6d1e
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-02-04 12:14:10 +0330

    Fixed Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Loader = () => {
   return (
     <div className="flex-center h-screen w-full">
-      <Image src="/icons/loading-Circle.svg" alt="loading circle" width={50} height={50} />
+      <Image src="/icons/loading-circle.svg" alt="loading circle" width={50} height={50} />
     </div>
   )
 }
